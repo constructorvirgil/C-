f4451b9674bc62dfa77c2641b4f656d2d67d878e
constructorvirgil <38716951+constructorvirgil@users.noreply.github.com>
2019-07-12 11:41:38 +0800

    Add files via upload

---
+++ pipe_client.cpp
@@ -0,0 +1,42 @@
+﻿#include "iostream"
+#include "windows.h"
+
+using namespace std;
+#define PIPE_NAME L"\\\\.\\Pipe\\test"
+int  main()
+{
+	char buffer[1024];
+	DWORD WriteNum;
+
+	//如果当时服务器没有打开，那么这个函数会直接返回失败
+	if (WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER) == FALSE)
+	{
+		cout << "等待命名管道实例失败！" << endl;
+		return 0;
+	}
+
+	//服务器准备好环境后，才打开这个管道
+	HANDLE hPipe = CreateFile(PIPE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hPipe == INVALID_HANDLE_VALUE)
+	{
+		cout << "创建命名管道失败！" << endl;
+		CloseHandle(hPipe);
+		return 0;
+	}
+	cout << "与服务器连接成功！" << endl;
+	while (1)
+	{
+		cin >> buffer;
+		//WriteNum返回写入的字节数
+		if (WriteFile(hPipe, buffer, strlen(buffer), &WriteNum, NULL) == FALSE)
+		{
+			cout << "数据写入管道失败！" << endl;
+			break;
+		}
+	}
+
+	cout << "关闭管道！" << endl;
+	CloseHandle(hPipe);
+	system("pause");
+	return 0;
+}
---
+++ pipe_server.cpp
@@ -0,0 +1,46 @@
+﻿#include "iostream"
+#include "windows.h"
+using namespace std;
+#define PIPE_NAME L"\\\\.\\Pipe\\test" 
+int main()
+{
+	char buffer[1024];
+	DWORD ReadNum;
+	
+	HANDLE hPipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_DUPLEX, PIPE_TYPE_BYTE | PIPE_READMODE_BYTE, 1, 0, 0, 1000, NULL);
+	if (hPipe == INVALID_HANDLE_VALUE)
+	{
+		cout << "创建命名管道失败！" << endl;
+		CloseHandle(hPipe);
+		return 0;
+	}
+
+
+	//阻塞
+	//必须保证阻塞在这里的时候，客户端才WaitNamedPipe,CreateFile
+	if (ConnectNamedPipe(hPipe, NULL) == FALSE)
+	{
+		cout << "与客户机连接失败！" << endl;
+		CloseHandle(hPipe);
+		return 0;
+	}
+	cout << "与客户机连接成功！" << endl;
+
+	while (1)
+	{
+		//等待客户端发送信息，如果没有收到，则会阻塞在这里
+		if (ReadFile(hPipe, buffer, 1024, &ReadNum, NULL) == FALSE)
+		{
+			cout << "读取数据失败！" << endl;
+			break;
+		}
+
+		buffer[ReadNum] = 0;
+		cout << "读取数据:" << buffer << endl;
+	}
+
+	cout << "关闭管道！" << endl;
+	CloseHandle(hPipe);
+	system("pause");
+	return 0;
+}
